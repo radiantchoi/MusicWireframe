78a05c37ecd87a3e9cb8f797902f2624e98c3949
Gordon Choi <thefirate@gmail.com>
2021-06-12 18:28:45 +0900

    Build animated button and finger shadow

--- MusicWireframe/ViewControllers/ViewController.swift
+++ MusicWireframe/ViewControllers/ViewController.swift
@@ -27,8 +27,6 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
 }
 
 extension ViewController {
@@ -62,5 +60,48 @@ extension ViewController {
         }
         
         isPlaying.toggle()
+    }
+    
+    @IBAction func touchedDown(_ sender: UIButton) {
+        let buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            buttonBackground.alpha = 0.3
+            sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
+    }
+    
+    @IBAction func touchedUpInside(_ sender: UIButton) {
+        var buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            buttonBackground.alpha = 0.0
+            buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = CGAffineTransform.identity
+        }) { (_) in
+            buttonBackground.transform = CGAffineTransform.identity
+        }
     }
 }
